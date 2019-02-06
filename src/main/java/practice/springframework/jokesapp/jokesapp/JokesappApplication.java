5d705274f3f18e9194723e3c3dc7ea57acdbff6f
Estel Seong <estel.castle@gmail.com>
2019-02-06 19:49:21 +0900

    Spring XML Configuration
    - xml-config.xml + @ImportResource

--- src/main/java/practice/springframework/jokesapp/jokesapp/JokesappApplication.java
+++ src/main/java/practice/springframework/jokesapp/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package practice.springframework.jokesapp.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chunk-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
--- src/main/java/practice/springframework/jokesapp/jokesapp/config/ChunkConfiguration.java
+++ src/main/java/practice/springframework/jokesapp/jokesapp/config/ChunkConfiguration.java
@@ -2,13 +2,8 @@ package practice.springframework.jokesapp.jokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class ChunkConfiguration {
 
-    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
